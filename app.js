877178d6e783a65f816eecc6508b59082ec26eb5
Dylan Wright <sealsurlaw@gmail.com>
2019-02-13 21:15:24 -0800

    Finished lab2

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-console.log('Hello World');
+const express = require('express');
+const app = express();
+const axios = require('axios');
+const port = 3000;
+
+app.get('/network', (req, res) => {
+    axios.get('http://localhost:3001/hello')
+        .then(resp => {
+            res.send(resp.data);
+        })
+        .catch(err => {
+            res.send(err);
+        })
+});
+
+app.listen(port);
